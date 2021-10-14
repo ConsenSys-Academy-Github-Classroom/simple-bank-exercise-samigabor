f76ace7b3476ab02e6e268d3ce1d90eb56c1c867
s-gabor <ing.gabor@yahoo.com>
2021-10-14 19:03:32 +0300

    add state variables visibility

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -14,16 +14,16 @@ contract SimpleBank {
     
     // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
-    mapping (address => uint) balances ;
+    mapping (address => uint) private balances ;
     
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
-    address owner = msg.sender;
+    address public owner = msg.sender;
     
     /* Events - publicize actions to external listeners
      */
